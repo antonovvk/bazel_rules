92d68c1b116f1d1335c0cf570be5ddf4f5645e5b
Vladimir Antonov <antonovvk@yandex.ru>
2018-07-18 08:31:21 +0300

    Moving stamping-related stuff to https://github.com/antonovvk/bazel_stamp

--- BUILD
+++
@@ -1,6 +0,0 @@
-load("@io_bazel_rules_go//go:def.bzl", "gazelle")
-
-gazelle(
-    name = "gazelle",
-    prefix = "github.com/antonovvk/bazel_rules",
-)
--- tools/workspace_status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-GIT_BRANCH=`git describe --all`
-GIT_TAG=`git describe --always --dirty`
-
-echo "STABLE_GIT_TAG $GIT_TAG on $GIT_BRANCH"
-echo "STABLE_BUILD_DATE `date`"
